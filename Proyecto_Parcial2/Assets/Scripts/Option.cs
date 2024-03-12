d68a3715bc11cf2b85bbe56336b0ac4d13b05b76
Leonardo Rosas <156142692+McFry3000@users.noreply.github.com>
2024-03-12 17:36:46 -0600

    Update Option.cs

--- Proyecto_Parcial2/Assets/Scripts/Option.cs
+++ Proyecto_Parcial2/Assets/Scripts/Option.cs
@@ -8,7 +8,8 @@ public class Option : MonoBehaviour
 {
     public int OptionId;
     public string OptionName;
-
+    
+//se inicia con las opciones de las primeras preguntas, obteniendo el componente Text Mesh Pro para ponerle las preguntas
     public void Start()
     {
         transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
@@ -18,11 +19,12 @@ public class Option : MonoBehaviour
     {
         
     }
-
+//se cambia el texto en UI con las opciones de las siguientes preguntas, de igual manera usando el componente Text Mesh Pro
     public void Updatetext()
     {
         transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
     }
+    //Checa si se seleccciona una opcion en la UI, usando el script de LevelManager
     public void SelectOptions()
     {
         LevelManager.Instance.setPlayerAnswer(OptionId);
