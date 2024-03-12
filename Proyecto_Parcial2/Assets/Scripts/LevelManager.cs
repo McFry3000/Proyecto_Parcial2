fff721c1a5269ed2de0e31930e6905fa33ccc9e1
Leonardo Rosas <156142692+McFry3000@users.noreply.github.com>
2024-03-12 17:53:15 -0600

    Update LevelManager.cs

--- Proyecto_Parcial2/Assets/Scripts/LevelManager.cs
+++ Proyecto_Parcial2/Assets/Scripts/LevelManager.cs
@@ -46,12 +46,14 @@ public class LevelManager : MonoBehaviour
     {
         //Establecemos la cantidad de preguntas en la leccion
         questionAmount = Lesson.leccionList.Count;
+        //Crga la pregunta inicial
         LoadQuestion();
+        //checa si el jugador tiene una opcion seleccionada
         CheckPlayerState();
     }
 
 
-
+//se carga la pregunta, teniendo en cuenta si el jugador yacompleto la pregunta anterior
    private void LoadQuestion()
     {
 
@@ -69,6 +71,7 @@ public class LevelManager : MonoBehaviour
 
             for(int i =0; i < currentLesson.opciones.Count; i++)
             {
+            //sirve para iterar a traves de una lista de opciones, usando sus identificadores provenientes desde el script option
                 opciones[i].GetComponent<Option>().OptionName = currentLesson.opciones[i];
                 opciones[i].GetComponent<Option>().OptionId =i;
                 opciones[i].GetComponent<Option>().Updatetext();
@@ -84,20 +87,24 @@ public class LevelManager : MonoBehaviour
 
     public void NextQuestion()
     {
+    //se checa la respuesta que selecciono el jugador
         if (CheckPlayerState())
         {
             if (currentQuestion < questionAmount)
             {
+            //vhecamos si la pregunta es correcta o no, dependiendo de esto, se abre un "if"
                 bool isCorrect = currentLesson.opciones[CorrectAnswerfromUser] == correctAnswer;
 
                 AnswerContainer.SetActive(true);
                 if(isCorrect)
                 {
+                //si la respuesta es correcta, semostrara en la UI el color verde dentro de un recuadro vacio, junto con el texto siguiente
                     AnswerContainer.GetComponent<Image>().color = Green;
                     Questiongood.text="Respuesta correcta. " + question + ": " + correctAnswer;
                 }
                 else
                 {
+                //si la resouesta es incorrecta, se mostrara en la UI el color rojo dentro del recuadro vacio, junto con el texto siguiente
 
                     AnswerContainer.GetComponent<Image>().color = Red;
                     Questiongood.text = "Respuesta Incorrecta. " + question + ": " + correctAnswer;
@@ -120,15 +127,15 @@ public class LevelManager : MonoBehaviour
             }
         }
     }
-
+//funcion que inicia la corrutina, la cual funciona en paralelo al codigo anterior
     private IEnumerator ShowResultAndLoadQuestion(bool isCorrect)
     {
-        yield return new WaitForSeconds(2.5f); //Ajusta el tiempo
+        yield return new WaitForSeconds(2.5f); //Ajusta el tiempo para mostrar el resultado
 
         //Oculta el contenedor de respuestas
         AnswerContainer.SetActive(false);
 
-        //cargar pregunta
+        //cargar pregunta siguiente
         LoadQuestion();
 
         //Activar el boton despues de mostrar el resultado
@@ -138,16 +145,19 @@ public class LevelManager : MonoBehaviour
         CheckPlayerState();
     }
 
-
+//se asgina la respuesta del jugador
     public void setPlayerAnswer(int _answer)
     {
+    /7actualiza la respuesta del jugador con el valorproporcionado
         CorrectAnswerfromUser = _answer;
     }
-
+//funcion que evalua si el juagdor interactua con el boton para hacer el modo hover 
     public bool CheckPlayerState()
     {
+    //si el jugador interactua con el boton, camvbiara de color
         if (CorrectAnswerfromUser != 9)
         {
+        
             checkbutton.GetComponent<Button>().interactable = true;
             checkbutton.GetComponent<Image>().color = Color.white;
             return true;
