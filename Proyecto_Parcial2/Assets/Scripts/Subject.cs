e79acf9e396c90e33f382d9ff7e420f00f7628be
Leonardo Rosas <156142692+McFry3000@users.noreply.github.com>
2024-03-12 17:39:47 -0600

    Update Subject.cs

--- Proyecto_Parcial2/Assets/Scripts/Subject.cs
+++ Proyecto_Parcial2/Assets/Scripts/Subject.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="new Subject", menuName = "ScriptableObjects/NewLesson", order =1)]
 public class Subject : ScriptableObject
 {
+//Crea un Scriptable Object que sirve para crear las lecciones, asi como las preguntas y las opciones de las mismas, la informacio de dicho SO puede ser alterada sin niguna preocupacion de que el siguiente codigo se vea afectado
     [Header("GameObject Configuration")]
     public int Lesson = 0;
 
